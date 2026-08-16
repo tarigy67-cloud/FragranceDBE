c4d7f19eea48dcdc213f361cb9146145458329b7
Tarig Bashari <tarigy.67@gmail.com>
2026-08-16 18:03:45 +0100

    This is the inital version where i finished the backend and did login/registration for the frontend

--- frontend/src/pages/Perfume.jsx
+++ frontend/src/pages/Perfume.jsx
@@ -1,19 +1,13 @@
 import {useState} from 'react'
 import {useParams} from 'react-router-dom'
 
-export default function Register(){
+export default function Perfume(){
 
 
 
 
 return(<> 
-
-
-
-
-
-
-
+Perfume Page
 
 </>)
 }
